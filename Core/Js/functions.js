5f28604438bf63c753df9323bccad6fa30cf27f0
Waldo Cosman <waldo_cosman@hotmail.com>
2015-10-13 15:37:50 +0200

    Sticky footer in separate file

--- Core/Js/functions.js
+++ Core/Js/functions.js
@@ -1,7 +1,4 @@
-$(window).resize(function ()
-{
-    StickyFooter();
-});
+
 
 $(document).ready(function ()
 {
@@ -47,43 +44,7 @@ $(document).ready(function ()
     });
 
 
-    //--Call Sticky Footer function
-    StickyFooter();
+
 
 });
-/**
- * resize Footer
- * footer back to the ground
- *
- */
 
-function StickyFooter()
-{
-    var intHeaderHeight = $("div.pagehead").outerHeight(true);
-    var intHeaderHeight = $("div.pagehead .logo").outerHeight(true);
-    console.log(intHeaderHeight);
-
-    //--Get height of texture-container
-    var intContentHeight = $("div.content").outerHeight(true);
-
-    //--Get height of the window
-    var intWindowHeight = $(window).outerHeight();
-
-    //--Get height of the footer
-    var intHeaderHeight = $("div.pagehead").outerHeight(true);
-    var intFooterHeight = $("div.pagefoot").outerHeight(true);
-    var intFooterMargin = parseInt($("div.pagefoot").css('margin-top'));
-
-    //--Check if windowheight is smaller then the main height
-    if (intWindowHeight < intContentHeight + intFooterHeight + intHeaderHeight - intFooterMargin)
-    {
-        intWindowHeight = intContentHeight + intFooterHeight + intHeaderHeight;
-    }
-    //--Check if the container is smaller then the window height - footer
-    if (intContentHeight <= intWindowHeight - intFooterHeight - intHeaderHeight)
-    {
-        $("div.content").outerHeight(intWindowHeight - intFooterHeight - intHeaderHeight - intFooterMargin + "px");
-
-    }
-
-}
